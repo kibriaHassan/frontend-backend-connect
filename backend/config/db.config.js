c1102d93c69c19dbbbaba8a36cd4487a323116ed
kibriahassan <kibriahassan3500@gmail.com>
2026-05-18 13:04:55 +0600

    db.config updates

--- backend/config/db.config.js
+++ backend/config/db.config.js
@@ -1,7 +1,6 @@
 const mongoose = require('mongoose');
 const dbConnect = async () => {
     try {
-        
         await mongoose.connect(process.env.MONGO_URI)
         console.log("Database connected")
     } catch (error) {
